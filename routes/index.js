77e25c303f2863892ff6e73d55331076024ecc87
Felipe Mantilla <andres.mantilla@gorillalogic.com>
2022-05-25 09:15:07 -0500

    [feature] add sync visitor stats and sync cancellations into the routes

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,14 @@ const router = require("express").Router();
 const syncPlayerProps = require("./sync-player-props");
 const syncOddsBoosts = require("./sync-odds-boosts");
 const syncUserStats = require("./sync-user-stats");
+const syncCancellations = require("./sync-cancellations");
+const syncVisitorStats = require("./sync-visitor-stats");
 
 router.use(syncPlayerProps);
 router.use(syncOddsBoosts);
 router.use(syncUserStats);
+router.use(syncCancellations);
+router.use(syncVisitorStats);
 
 module.exports = {
   router,
